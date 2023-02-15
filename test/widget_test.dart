b6d467acbb9ab5efd3a8b9eba616d7cc7ea54b47
Paul <paulmuchwat@gmail.com>
2023-02-15 11:04:59 +0300

    fix error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:slide_switcher/main.dart';
+import 'package:slide_switcher_ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
